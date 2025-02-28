afb716d40ba98f13c2feb88bc263c227a2e7e773
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2025-02-28 15:48:30 +0000

    NoNewGlobals for OpenSSL-related structures (#2006)
    These changes were anticipated in Bug 5390 fix (recent commit c565067):
    https://bugs.squid-cache.org/show_bug.cgi?id=5390#c16
    
    They eliminate all known OpenSSL-related globals:
    
    * Security::ProxyOutgoingConfig
    * Ssl::SquidUntrustedCerts
    * Ssl::TheGeneratorRequests
    * Ssl::TheGlobalContextStorage
    
    Also applied AAA and range-based `for` loop upgrades to modified lines.

--- src/main.cc
+++ src/main.cc
@@ -859,7 +859,7 @@ mainReconfigureStart(void)
     htcpClosePorts();
 #endif
 #if USE_OPENSSL
-    Ssl::TheGlobalContextStorage.reconfigureStart();
+    Ssl::TheGlobalContextStorage().reconfigureStart();
 #endif
 #if USE_AUTH
     authenticateReset();
