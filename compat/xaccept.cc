268dd88c6b506e85c8b30df97e66efb66be83d11
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2025-04-07 18:43:34 +0100

    Add special handling for WSAECONNRESET
    Co-authored-by: Amos Jeffries <yadij@users.noreply.github.com>

--- compat/xaccept.cc
+++ compat/xaccept.cc
@@ -22,11 +22,17 @@ xaccept(int s, struct sockaddr *a, socklen_t *l)
 {
     SOCKET result;
     if ((result = accept(_get_osfhandle(s), a, l)) == INVALID_SOCKET)
-    {
-        if (WSAEMFILE == (errno = WSAGetLastError()))
+        errno = WSAGetLastError();
+        if (errno == WSAECONNRESET) {
+            // for Comm::TcpAcceptor::acceptInto() special handling
+            // Client gave up, or was accept()'d by other SMP worker.
+            // Linux POSIX API uses the Connection Aborted code for this case.
+            errno = ECONNABORTED;
+        } else if (errno == WSAEMFILE) {
+            // too many open sockets, cannot accept another right now
             errno = EMFILE;
+        }
         return -1;
-    }
     else
         return _open_osfhandle(result, 0);
 }
